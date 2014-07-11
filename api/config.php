67ef5f0b494c946b116a9de87948ce60db23b03c
tech <tech@drupal1.tacoma.uw.edu>
2014-07-11 09:44:18 -0700

    Initial commit of the StrongSchools site.

---
+++ api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'bLBITjVTKjrjNWG7ZwvHabZ9n');
+    define('CONSUMER_SECRET', 'AUFiUBFTlhh2vm4OBPj81IaHfUaMMNivDodBbwzFrQGMFvQoZF');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '2411447658-nsopynBwTdQq4UPTqkNfFwXRUV5tIBSQnTAxhjD');
+    define('ACCESS_SECRET', 'Nl3QBduroPgEzR7bUFZ1OHMlDvS4khVrg5T4toksx4KZ0');
